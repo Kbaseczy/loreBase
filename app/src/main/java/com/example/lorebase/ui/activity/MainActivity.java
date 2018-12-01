f0429d76d1296c916f5673fbc6c1760837eb0189
ihdoB <1073183792@qq.com>
2018-12-01 12:38:22 +0800

    1

--- app/src/main/java/com/example/lorebase/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/lorebase/ui/activity/MainActivity.java
@@ -254,6 +254,8 @@ public class MainActivity extends BaseActivity implements BottomNavigationView.O
                 break;
             case R.id.nav_setting:
                 Toast.makeText(this, "test click setting", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this,SettingsActivity.class));
+                overridePendingTransition(R.animator.go_in, R.animator.go_out);
                 break;
             case R.id.nav_about_us:
                 startActivity(new Intent(getApplicationContext(), AboutUsActivity.class));
