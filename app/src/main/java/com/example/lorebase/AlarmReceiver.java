6745ca36fa851baaf8268415314e92101cb0f5a1
Kbaseczy <40874618+Kbaseczy@users.noreply.github.com>
2019-02-12 11:04:48 +0800

    alarmreceiver

---
+++ app/src/main/java/com/example/lorebase/AlarmReceiver.java
@@ -0,0 +1,13 @@
+package com.example.lorebase;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context,AlarmService.class));
+    }
+}
