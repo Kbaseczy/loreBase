f22edd82dcecb226a4a7e06f8a87db5da4edc898
ihdoB <1073183792@qq.com>
2019-03-25 20:46:22 +0800

    bug修复
    设置界面和主界面切换时，
    登陆态丢失原因：主界面的onDestroy中执行了注销。
    解决办法：取消注销操作。

--- app/src/main/java/com/example/lorebase/AlarmService.java
+++ app/src/main/java/com/example/lorebase/AlarmService.java
@@ -40,7 +40,7 @@ public class AlarmService extends Service {
         int importance = NotificationManager.IMPORTANCE_HIGH;
         createNotificationChannel(channelId,name,importance);
         AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        int anHour = 4 * 60 * 60 * 1000;  //  用户手动设置时间
+        int anHour = 60 * 60 * 1000;  //  用户手动设置时间
 //        int anHour = 10 * 1000;  //10s.
         long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
         Intent i = new Intent(this, AlarmService.class);
